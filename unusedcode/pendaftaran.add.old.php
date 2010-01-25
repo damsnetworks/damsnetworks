b4919f7b6227924b6aabc705cdba5c8c0cbaedb6
Adam Ramadhan <adamramadhan@live.com>
2010-01-25 23:45:30 +0700

    added for documentations :)

---
+++ unusedcode/pendaftaran.add.old.php
@@ -0,0 +1,11 @@
+/*
+$sql="INSERT INTO users (Username, OwnerFn, OwnerLn, Password, Phone, Partnership)
+VALUES
+('$_POST[Username]','$_POST[OwnerFn]','$_POST[OwnerLn]','$_POST[Password]','$_POST[Phone]','$_POST[Partnership]')";
+if (!mysql_query($sql,$connectdb))
+{
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+?>
+*/
