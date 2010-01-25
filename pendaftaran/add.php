21b4ce59cd2b3aed2a6a8db7277cb5fb78adffdb
Adam Ramadhan <adamramadhan@live.com>
2010-01-25 23:44:27 +0700

    Secureing ADD php adding some magic on name takeings :p

---
+++ pendaftaran/add.php
@@ -0,0 +1,30 @@
+<?php 
+include("../f/db.php");
+if(!empty($_POST['Username']) && !empty($_POST['Password']))
+{
+  $Username = mysql_real_escape_string($_POST['Username']);
+  $Password = md5(mysql_real_escape_string($_POST['Password']));
+  $checkusername = mysql_query("SELECT * FROM users WHERE Username = '".$Username."'");
+    if(mysql_num_rows($checkusername) == 1)
+     {
+      echo "<h1>Error</h1>";
+      echo "<p>Sorry, that username is taken. Please go back and try again.</p>";
+     }
+     else
+     {
+      $registerquery = mysql_query("INSERT INTO users (Username, OwnerFn, OwnerLn, Password, Phone, Partnership) VALUES('$_POST[Username]','$_POST[OwnerFn]','$_POST[OwnerLn]','$_POST[Password]','$_POST[Phone]','$_POST[Partnership]')");
+      if($registerquery)
+        {
+          echo "<h1>Success</h1>";
+          echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
+        }
+        else
+        {
+        echo "<h1>Error</h1>";
+        echo "<p>Sorry, your registration failed. Please go back and try again.</p>";    
+        }    	
+     }
+}
+
+  ?>
+
