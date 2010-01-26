d6f56d08ca576f848d8e3c8812437476c43ae3e6
Adam Ramadhan <adamramadhan@live.com>
2010-01-26 17:56:47 +0700

    cleanups /f -> /inc , a better nameing credits @pungkerz

--- f/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "project-10";
-$connectdb = mysql_connect("$server","$user","$pass") or die("<p>gatot!, dengan server<p>");
-mysql_select_db("$db") or die("<p>gatot!, dengan db<p>");
-?>
