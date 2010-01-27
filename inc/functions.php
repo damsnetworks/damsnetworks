e7e92770ad3a6fd06bd3556ddbb01f2c26f0c264
Pungkerz <pungkerz@gmail.com>
2010-01-27 14:35:17 +0700

    Add todo list functions

---
+++ inc/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/* kumpulan functions yg dibutuhin
+lists:
+
+- sanitize field ($_POST, $_GET, $_COOKIE, dll)
+- validasi username
+- validasi email
+- validasi url
+- send email
+
+*/
+
+// end of file
