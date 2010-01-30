26656db0c7a9941564666d9c7f6b266a8197172b
Adam Ramadhan <adamramadhan@live.com>
2010-01-30 15:13:33 +0700

    some tests

--- tests/tests.php
+++ tests/tests.php
@@ -36,10 +36,49 @@ class TMPL
 }
 
 $showMenu = new TMPL();
-$showMenu->box(10)
+$showMenu->box(10);
 //OVEWRITE
 
 
 
+function getUserBrowser()
+{
+	$uBrowser = $_SERVER['HTTP_USER_AGENT'];
+	
+	if(preg_match('/MSIE/i',$uBrowser))
+	{
+		$sBrowser = "ie";
+	}
+	elseif(preg_match('/Firefox/i',$uBrowser))
+	{
+		$sBrowser = "firefox";
+	}
+	// Chrome beta pake safari jadi gw taro diatas
+	elseif(preg_match('/Chrome/i',$uBrowser))
+	{
+		$sBrowser = "chrome";
+	}
+	elseif(preg_match('/Safari/i',$uBrowser))
+	{
+		$sBrowser = "safari";
+	}
+	elseif(preg_match('/Flock/i',$uBrowser))
+	{
+		$sBrowser = "flock";
+	}
+	elseif(preg_match('/Opera/i',$uBrowser))
+	{
+		$sBrowser = "opera";
+	}
+	return $sBrowser;
+} 
 
 ?>
+
+
+
+
+
+	
+</div>
+
